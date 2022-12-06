6bcd21aab3dfe48f7debb60a2383c6c6f7ef87c0
Laurrence <laurrence.j4f@gmail.com>
2022-12-06 23:49:56 +0800

    wip logout button

--- src/WebUI/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ src/WebUI/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthorizeService } from '../../api-authorization/authorize.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
+
+  constructor(private authService: AuthorizeService,
+    private router: Router) { }
+
   isExpanded = false;
+  isLoggedIn = false
 
   collapse() {
     this.isExpanded = false;
@@ -14,5 +21,27 @@ export class NavMenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  ngOnInit(): void {
+    this.toggle();
+    this.collapse();
+
+    if (this.authService.isLoggedIn) {
+      let tmp = this.authService.getUserProfile()
+      if (tmp)
+        this.isLoggedIn = !this.isLoggedIn
+      //this.userRole = tmp.roleName
+    }
+  }
+
+  logout() {
+    //try {
+    //  this.accountService.logout().subscribe();
+    //} catch (err) {
+    //}
+
+    localStorage.clear();
+    this.router.navigateByUrl('/login');
   }
 }
